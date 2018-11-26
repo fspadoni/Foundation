2342491fbab6d137a00d33568f83ff0a20c5f98e
fspadoni <spadoni.federico@gmail.com>
2018-11-26 09:49:18 +0100

    [Foundation] add Platform config and define files

---
+++ foundation/src/PlatformConfig.h
@@ -0,0 +1,13 @@
+//
+//  PlatformConfig.h
+//  foundation
+//
+//  Created by Federico Spadoni on 24/08/17.
+//
+//
+
+#ifndef PlatformConfig_h
+#define PlatformConfig_h
+
+
+#endif /* PlatformConfig_h */
---
+++ foundation/src/PlatformDefine.h
@@ -0,0 +1,13 @@
+//
+//  PlatformDefine.h
+//  foundation
+//
+//  Created by Federico Spadoni on 24/08/17.
+//
+//
+
+#ifndef PlatformDefine_h
+#define PlatformDefine_h
+
+
+#endif /* PlatformDefine_h */
