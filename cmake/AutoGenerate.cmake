642d37f0aeeafb9db52dc770fed8906cfd34cb5e
fspadoni <spadoni.federico@gmail.com>
2018-12-07 19:55:02 +0100

    [CMAKE] add the the dependency of the tool generated code

---
+++ cmake/AutoGenerate.cmake
@@ -0,0 +1,56 @@
+
+include(CMakeParseArguments)
+
+function(build_autogen)
+
+    set(oneValueArgs
+
+        EXECUTABLE_TOOL
+        AUTOGEN_DIR 
+        AUTOGEN_POSTFIX      
+    )
+
+    set(multiValueArgs 
+
+        PROC_FILES
+        OUT_AUTOGEN_FILES
+        )
+
+    cmake_parse_arguments(ARGS  "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    # Generation tool invocation command:
+    #set( FOUNDATION_AUTOGEN_DIR "${CMAKE_CURRENT_BINARY_DIR}/autogen")
+    file (MAKE_DIRECTORY ARGS_AUTOGEN_DIR)
+
+    set(AUTOGEN_FILES "")
+
+    foreach (proc_file ${ARGS_PROC_FILES})
+
+        get_filename_component(DIRECTORY_NAME ${proc_file} DIRECTORY)
+        get_filename_component(BASE_NAME ${proc_file} NAME_WE)
+        get_filename_component(EXTENSION ${proc_file} EXT)
+
+        # make sure the path is absolute
+        if (NOT IS_ABSOLUTE ${proc_file})
+            set(DIRECTORY_NAME "${ARGS_FOUNDATION_AUTOGEN_DIR}/${DIRECTORY_NAME}")
+        endif ()
+
+        file(RELATIVE_PATH REL_AUTOGEN_FILE ${ARGS_AUTOGEN_DIR} "${DIRECTORY_NAME}/${BASE_NAME}_${ARGS_AUTOGEN_POSTFIX}.cpp" )
+        set( AUTOGEN_FILE  "${ARGS_AUTOGEN_DIR}/${REL_AUTOGEN_FILE}" )
+
+        # that's by default so it should not be necesssary
+        set_source_files_properties(${AUTOGEN_FILE} PROPERTIES GENERATED TRUE)
+
+        list(APPEND AUTOGEN_FILES ${AUTOGEN_FILE} )
+    endforeach ()
+
+    add_custom_command(
+        OUTPUT ${AUTOGEN_FILES}
+        COMMAND ${EXECUTABLE_TOOL}   ${ARGS_AUTOGEN_DIR}  ${ARGS_PROC_FILES} 
+        MAIN_DEPENDENCY ${PROC_FILES}
+        COMMENT " .. Generating files with ${ARGS_EXECUTABLE_TOOL} "
+    )
+
+    set(${ARGS_OUT_AUTOGEN_FILES} "${AUTOGEN_FILES}" PARENT_SCOPE)
+
+endfunction()
--- tools/main.cpp
+++ tools/main.cpp
@@ -1,7 +1,83 @@
 #include <iostream>
+#include <fstream>
+#include <exception>
 
-int main(int argc, char** argv)
+
+/*
+ * Get File Name from a Path with or without extension
+ */
+std::string getFileName(std::string filePath, bool withoutExtension = true, char seperator = '/')
 {
-    std::cout<<"Hello, World!\n";
+    // Get last dot position
+    std::size_t dotPos = filePath.rfind('.');
+    std::size_t sepPos = filePath.rfind(seperator);
+    
+    if(sepPos != std::string::npos)
+    {
+        return filePath.substr(sepPos + 1, ((dotPos != std::string::npos && withoutExtension) ?  dotPos : filePath.size() ) - sepPos - 1);
+    }
+    return "";
+}
+
+std::string removeExt(std::string filePath)
+{
+    // Get last dot position
+    std::size_t dotPos = filePath.rfind('.');
+    
+    if(dotPos != std::string::npos)
+    {
+        return filePath.substr(0, dotPos);
+    }
+    return filePath;
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    // parse command line
+    try
+    {
+    }
+    catch (std::exception &e)
+    {
+        
+    }
+    catch (...)
+    {
+    }
+    
+    std::cout<<"Hello, testTool starts!\n";
+    
+    std::string inputFile;
+    std::string outputDir;
+    
+    for (int i = 0; i < argc; ++i)
+    {
+        std::cout << argv[i] << "\n";
+        if (i==1)
+        {
+            // dir
+            outputDir = std::string(argv[i]);
+//            inputFile = getFileName(inputFile, true);
+        }
+        else if (i>=2)
+        {
+            // dir
+            inputFile = std::string(argv[i]);
+            inputFile = removeExt(inputFile);
+            
+            std::string outputFile = outputDir + "/" + inputFile + "_autogen.cpp";
+            std::cout << "testTool "<<  i << ": write " << outputFile << "\n";
+            std::ofstream myfile;
+            myfile.open(outputFile);
+            myfile << "static void "+ getFileName(inputFile, true) + "_autogen() {}";
+            myfile.close();
+        }
+    }
+    
+    
+    std::cout<<"Hello, testTool ends!\n";
+    
     return 0;
 }
