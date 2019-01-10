f846671c7b86ea2eeffb766bfd22ec87e0efefdc
fspadoni <spadoni.federico@gmail.com>
2019-01-10 23:36:40 +0100

    [CMake] fix CMake file for foundation tool to include and link the LLVM and clang libs

--- tools/main.cpp
+++ tools/main.cpp
@@ -3,6 +3,32 @@
 #include <exception>
 
 
+#include <llvm/Support/TargetSelect.h>
+#include <clang/Tooling/CommonOptionsParser.h>
+#include <clang/Tooling/Tooling.h>
+#include <clang/Driver/Options.h>
+#include <llvm/Option/OptTable.h>
+
+static llvm::cl::OptionCategory ClangCheckCategory("foundationTool options");
+static std::unique_ptr<llvm::opt::OptTable> Options(clang::driver::createDriverOptTable());
+static llvm::cl::opt<bool>
+ASTDump("ast-dump", llvm::cl::desc(Options->getOptionHelpText(clang::driver::options::OPT_ast_dump)),
+        llvm::cl::cat(ClangCheckCategory));
+static llvm::cl::opt<bool>
+ASTList("ast-list", llvm::cl::desc(Options->getOptionHelpText(clang::driver::options::OPT_ast_list)),
+        llvm::cl::cat(ClangCheckCategory));
+static llvm::cl::opt<bool>
+ASTPrint("ast-print",
+         llvm::cl::desc(Options->getOptionHelpText(clang::driver::options::OPT_ast_print)),
+         llvm::cl::cat(ClangCheckCategory));
+static llvm::cl::opt<std::string> ASTDumpFilter(
+                                          "ast-dump-filter",
+                                          llvm::cl::desc(Options->getOptionHelpText(clang::driver::options::OPT_ast_dump_filter)),
+                                          llvm::cl::cat(ClangCheckCategory));
+static llvm::cl::opt<bool>
+Analyze("analyze", llvm::cl::desc(Options->getOptionHelpText(clang::driver::options::OPT_analyze)),
+        llvm::cl::cat(ClangCheckCategory));
+
 /*
  * Get File Name from a Path with or without extension
  */
@@ -76,6 +102,14 @@ int main(int argc, char *argv[])
         }
     }
     
+    // Initialize targets for clang module support.
+    llvm::InitializeAllTargets();
+    llvm::InitializeAllTargetMCs();
+    llvm::InitializeAllAsmPrinters();
+    llvm::InitializeAllAsmParsers();
+    
+    clang::tooling::CommonOptionsParser OptionsParser(argc, (const char**)argv, ClangCheckCategory);
+    clang::tooling::ClangTool Tool(OptionsParser.getCompilations(), OptionsParser.getSourcePathList());
     
     std::cout<<"Hello, testTool ends!\n";
     
